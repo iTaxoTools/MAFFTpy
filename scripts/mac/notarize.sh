253fd2fc1ece9f4e1c3a57a8d1991a5e7c32da56
Stefanos Patmanidis <stefanpatman91@gmail.com>
2025-08-04 14:07:58 +0300

    Remove gui and pyinstaller scripts

--- scripts/mac/notarize.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-xcrun notarytool submit "$APP_IMAGE" \
-  --keychain-profile "AC_PASSWORD" \
-  --wait
-xcrun stapler staple "$APP_IMAGE"
-
-# verify
-spctl -a -t open -vvv --context context:primary-signature "$APP_IMAGE"
--- src/itaxotools/__pyinstaller/hook-itaxotools.mafftpy.gui.py
+++
@@ -1,6 +0,0 @@
-# Include all package data
-
-from PyInstaller.utils.hooks import collect_data_files
-
-datas = collect_data_files("itaxotools.mafftpy")
-datas += collect_data_files("itaxotools.mafftpy.gui")
