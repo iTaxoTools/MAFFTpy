86d8c85a99c3c457aa352162205375fc1fb1b37c
StefanPatman <stefanpatman91@gmail.com>
2025-08-23 16:52:40 +0300

    Fix fuse wheels script

--- tools/fuse-wheels.py
+++ tools/fuse-wheels.py
@@ -8,19 +8,24 @@ def fuse_wheelhouse(wheelhouse: Path):
     assert wheelhouse.is_dir()
     arm64: set[str] = set()
     x86_64: set[str] = set()
+    arm64_names: dict[str, str] = {}
+    x86_64_names: dict[str, str] = {}
     for path in wheelhouse.iterdir():
         if "macosx" not in path.name:
             continue
         stem = path.name.split("-macosx")[0]
         if "11_0_arm64" in path.name:
+            arm64_names[stem] = path.name
             arm64.add(stem)
         elif "10_9_x86_64" in path.name:
+            x86_64_names[stem] = path.name
             x86_64.add(stem)
         elif "10_13_x86_64" in path.name:
+            x86_64_names[stem] = path.name
             x86_64.add(stem)
     for target in arm64 & x86_64:
-        to_wheel = f"{target}-macosx_11_0_arm64.whl"
-        from_wheel = f"{target}-macosx_10_9_x86_64.whl"
+        to_wheel = arm64_names[target]
+        from_wheel = x86_64_names[target]
         out_wheel = f"{target}-macosx_11_0_universal2.whl"
         print(f"Fusing {to_wheel} & {from_wheel} --> {out_wheel}")
         to_path = str(wheelhouse / to_wheel)
